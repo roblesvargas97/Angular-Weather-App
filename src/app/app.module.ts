386e71c58e5a3600b575bee2b1871375761de98c
roblesvargas97 <roblesvargas97@gmail.com>
2022-05-30 21:29:34 -0500

    :construction: work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SearcherComponent } from './components/searcher/searcher.component';
+import { InstructionsComponent } from './components/instructions/instructions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearcherComponent,
+    InstructionsComponent
   ],
   imports: [
     BrowserModule,
